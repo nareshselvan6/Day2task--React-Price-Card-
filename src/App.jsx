3f7ec4c5e5e933d4b00e8a5e8662478ae8ee27b3
nareshselvan6 <nareshselvan6@gmail.com>
2024-05-04 14:17:16 +0530

    React-Price-Card

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PriceCard from "./Components/PriceCardTask/PriceCard.jsx"
+
+
+function App() {
+  
+  return (
+    <>
+  
+   <PriceCard />
+    </>
+  )
+}
+
+export default App
